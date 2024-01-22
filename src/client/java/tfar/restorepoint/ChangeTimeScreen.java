a17efb6beec1d8aa64292a8ab38a25d78c314a24
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-01-22 08:22:47 -0500

    add a screen and slider

--- src/client/java/tfar/restorepoint/ChangeTimeScreen.java
+++ src/client/java/tfar/restorepoint/ChangeTimeScreen.java
@@ -1,17 +1,39 @@
 package tfar.restorepoint;
 
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import tfar.restorepoint.mixin.client.FabricSlider;
 
 public class ChangeTimeScreen extends Screen {
+
+    private static final ResourceLocation DEMO_BACKGROUND_LOCATION = new ResourceLocation("textures/gui/demo_background.png");
+
     protected ChangeTimeScreen(Component title) {
         super(title);
     }
 
+    protected int imageWidth = 176;
+    protected int imageHeight = 166;
+
     @Override
     protected void init() {
         super.init();
-        addWidget(new FabricSlider(1,1,1,1,Component.empty(),Component.empty(),0,1,1,0,0,true));
+
+        int i = (this.width - this.imageWidth) / 2;
+        int j = (this.height - this.imageHeight) / 2;
+
+        addWidget(new FabricSlider(i + 38,j + 46,100,20,Component.empty(),Component.empty(),
+                0,24000,120,1,0,true));
     }
+
+    @Override
+    public void renderBackground(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        super.renderBackground(guiGraphics, mouseX, mouseY, partialTick);
+        int i = (this.width - 248) / 2;
+        int j = (this.height - 166) / 2;
+        guiGraphics.blit(DEMO_BACKGROUND_LOCATION, i, j, 0, 0, 248, 166);
+    }
+
 }
--- src/client/java/tfar/restorepoint/RestorePointClient.java
+++ src/client/java/tfar/restorepoint/RestorePointClient.java
@@ -1,11 +1,30 @@
 package tfar.restorepoint;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
 
 public class RestorePointClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-
+		UseItemCallback.EVENT.register(this::interact);
 	}
+
+	InteractionResultHolder<ItemStack> interact(Player player, Level world, InteractionHand hand) {
+		if (player.getItemInHand(hand).getItem() == Items.STICK) {
+			Minecraft.getInstance().setScreen(new ChangeTimeScreen(Component.empty()));
+			return InteractionResultHolder.success(player.getItemInHand(hand));
+		}
+		return InteractionResultHolder.pass(ItemStack.EMPTY);
+	}
+
+
 }
